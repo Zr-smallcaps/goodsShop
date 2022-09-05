6219238c504d48a5ae92724021e7a7229af82c47
smallcaps <1827662823@qq.com>
2022-09-05 11:44:02 +0800

    layout完成

---
+++ unpackage/dist/dev/mp-weixin/pages/car/car.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/car/car"],{
+
+/***/ 24:
+/*!****************************************************************!*\
+  !*** E:/U/code/goodsShop/main.js?{"page":"pages%2Fcar%2Fcar"} ***!
+  \****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _car = _interopRequireDefault(__webpack_require__(/*! ./pages/car/car.vue */ 25));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_car.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 25:
+/*!*********************************************!*\
+  !*** E:/U/code/goodsShop/pages/car/car.vue ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'E:\\U\\code\\goodsShop\\pages\\car\\car.vue'");
+
+/***/ })
+
+},[[24,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/car/car.js.map
